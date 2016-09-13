3761eb1619a0cac355b6c5a9769d532fd7629139
zhaozeyang <zhaozeyang@zhaozeyangdeMacBook-Pro.local>
2016-09-13 10:21:32 +0800

    修改关于页面样式

--- tox/src/main/java/com/thinksky/tox/AboutActivity.java
+++ tox/src/main/java/com/thinksky/tox/AboutActivity.java
@@ -1,18 +1,36 @@
 package com.thinksky.tox;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
-
 import com.thinksky.holder.BaseBActivity;
 
 public class AboutActivity extends BaseBActivity {
-    private TextView fanhui;
+  private View fanhui;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting_guanyu);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.setting_guanyu);
+
+    fanhui = findViewById(R.id.guanyuqibai_back);
+    fanhui.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        finish();
+      }
+    });
+    PackageManager manager = getPackageManager();
+    try {
+      PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
+      String versionName = info.versionName;
+      ((TextView) findViewById(R.id.version)).setText(versionName);
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
     }
+  }
 
 
 }
